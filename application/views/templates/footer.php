4001c58098ef21d0adfe53a3f39721528f8bf6ca
mirivlad <mirivlad@gmail.com>
2013-03-28 17:26:44 +0900

    Удалил Flexi_auth

---
+++ application/views/templates/footer.php
@@ -0,0 +1,8 @@
+	</div>
+    </div>
+</div>
+<script src="/assets/js/jquery-1.9.1.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<p class="muted" style="padding: 1em; padding-left: 3em;">Render time <strong>{elapsed_time}</strong> sec.</p>
+</body>
+</html>
