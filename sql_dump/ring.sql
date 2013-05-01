9e91eebb27a97be49a2ac5d0f6645262eaaf27fe
mirivlad <mirivlad@mirv-home>
2013-05-02 04:15:33 +0900

    Обновлён SQL файл дампа БД с изменениями для профиля пользователя.

--- sql_dump/ring.sql
+++ sql_dump/ring.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.7
+-- version 3.4.11.1deb2
 -- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Апр 11 2013 г., 20:20
--- Версия сервера: 5.5.30-log
--- Версия PHP: 5.4.13
+-- Хост: localhost
+-- Время создания: Май 02 2013 г., 04:14
+-- Версия сервера: 5.5.30
+-- Версия PHP: 5.4.4-14
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,13 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_general_ci NOT NULL,
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(150) NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text COLLATE utf8_general_ci NOT NULL,
+  `user_data` text NOT NULL,
   PRIMARY KEY (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,10 +43,10 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 
 CREATE TABLE IF NOT EXISTS `login_attempts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(40) COLLATE utf8_general_ci NOT NULL,
+  `ip_address` varchar(40) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -57,9 +57,9 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
 CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL,
-  `data` text COLLATE utf8_general_ci,
+  `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Дамп данных таблицы `permissions`
@@ -77,9 +77,9 @@ INSERT INTO `permissions` (`id`, `role_id`, `data`) VALUES
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL DEFAULT '0',
-  `name` varchar(30) COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Дамп данных таблицы `roles`
@@ -98,27 +98,27 @@ INSERT INTO `roles` (`id`, `parent_id`, `name`) VALUES
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL DEFAULT '1',
-  `username` varchar(25) COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(34) COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(34) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `banned` tinyint(1) NOT NULL DEFAULT '0',
-  `ban_reason` varchar(255) COLLATE utf8_general_ci DEFAULT NULL,
-  `newpass` varchar(34) COLLATE utf8_general_ci DEFAULT NULL,
-  `newpass_key` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
+  `ban_reason` varchar(255) DEFAULT NULL,
+  `newpass` varchar(34) DEFAULT NULL,
+  `newpass_key` varchar(32) DEFAULT NULL,
   `newpass_time` datetime DEFAULT NULL,
-  `last_ip` varchar(40) COLLATE utf8_general_ci NOT NULL,
+  `last_ip` varchar(40) NOT NULL,
   `last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `email`, `banned`, `ban_reason`, `newpass`, `newpass_key`, `newpass_time`, `last_ip`, `last_login`, `created`, `modified`) VALUES
-(1, 2, 'admin', '$1$bO..IR4.$CxjJBjKJ5QW2/BaYKDS7f.', 'admin@localhost.com', 0, NULL, NULL, NULL, NULL, '127.0.0.1', '2013-04-11 19:23:17', '2008-11-30 04:56:32', '2013-04-11 15:23:17'),
+(1, 2, 'admin', '$1$bO..IR4.$CxjJBjKJ5QW2/BaYKDS7f.', 'admin@localhost.com', 0, NULL, NULL, NULL, NULL, '127.0.0.1', '2013-05-01 22:39:41', '2008-11-30 04:56:32', '2013-05-01 13:39:41'),
 (2, 1, 'user', '$1$bO..IR4.$CxjJBjKJ5QW2/BaYKDS7f.', 'user@localhost.com', 0, NULL, NULL, NULL, NULL, '127.0.0.1', '2013-04-11 19:22:15', '2008-12-01 14:01:53', '2013-04-11 15:22:15');
 
 -- --------------------------------------------------------
@@ -128,13 +128,20 @@ INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `email`, `banned`,
 --
 
 CREATE TABLE IF NOT EXISTS `user_autologin` (
-  `key_id` char(32) COLLATE utf8_general_ci NOT NULL,
+  `key_id` char(32) NOT NULL,
   `user_id` mediumint(8) NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_general_ci NOT NULL,
-  `last_ip` varchar(40) COLLATE utf8_general_ci NOT NULL,
+  `user_agent` varchar(150) NOT NULL,
+  `last_ip` varchar(40) NOT NULL,
   `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`key_id`,`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user_autologin`
+--
+
+INSERT INTO `user_autologin` (`key_id`, `user_id`, `user_agent`, `last_ip`, `last_login`) VALUES
+('de8ece17487bd178aa133b460c8a77bf', 1, 'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0 Iceweasel/17.0.1', '127.0.0.1', '2013-05-01 13:39:41');
 
 -- --------------------------------------------------------
 
@@ -145,17 +152,25 @@ CREATE TABLE IF NOT EXISTS `user_autologin` (
 CREATE TABLE IF NOT EXISTS `user_profile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `country` varchar(20) COLLATE utf8_general_ci DEFAULT NULL,
-  `website` varchar(255) COLLATE utf8_general_ci DEFAULT NULL,
+  `country` varchar(20) DEFAULT NULL,
+  `city` varchar(255) NOT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) NOT NULL DEFAULT 'default.png',
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `sex` set('men','woman','not_set') NOT NULL DEFAULT 'not_set',
+  `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Дамп данных таблицы `user_profile`
 --
 
-INSERT INTO `user_profile` (`id`, `user_id`, `country`, `website`) VALUES
-(1, 1, NULL, NULL);
+INSERT INTO `user_profile` (`id`, `user_id`, `country`, `city`, `website`, `avatar`, `first_name`, `middle_name`, `surname`, `birthdate`, `sex`, `description`) VALUES
+(1, 1, '', '', '', 'default.png', '', '', '', '0000-00-00', '', '');
 
 -- --------------------------------------------------------
 
@@ -165,14 +180,14 @@ INSERT INTO `user_profile` (`id`, `user_id`, `country`, `website`) VALUES
 
 CREATE TABLE IF NOT EXISTS `user_temp` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(34) COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_general_ci NOT NULL,
-  `activation_key` varchar(50) COLLATE utf8_general_ci NOT NULL,
-  `last_ip` varchar(40) COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(34) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_key` varchar(50) NOT NULL,
+  `last_ip` varchar(40) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
