186d8420b5945b1afe577c4bc5c622cae4ea6745
mirivlad <mirivlad@gmail.com>
2013-05-04 03:08:46 +0900

    фикс адреса почты

--- application/views/contacts.php
+++ application/views/contacts.php
@@ -3,7 +3,7 @@ $this->load->view('templates/header');
 ?>
 	<div class="span10">
 	    <h1>{title}</h1>
-            <p><i class="icon-pencil"></i> <?= safe_mailto('me@my-site.com', 'Связаться с нами по почте')?></p>
+            <p><i class="icon-pencil"></i> <?= safe_mailto('admin@2084.su', 'Связаться с нами по почте')?></p>
             <p><img src="http://xkcd.ru/i/378_v1.png"></p>
         </div>
 <?php
